a05b8061422d5a387583f70948f678e51740215d
Jono Wenger <jono@elixxir.io>
2022-08-19 11:23:56 -0700

    Replace usage of filebuffer.Buffer with bytes.Reader and make File.Write work correctly

--- portableOS/types_js.go
+++ portableOS/types_js.go
@@ -10,22 +10,27 @@ package portableOS
 // This file is only compiled for WebAssembly.
 
 import (
-	"github.com/mattetti/filebuffer"
+	"bytes"
+	"github.com/pkg/errors"
+	"sync"
 )
 
 // JsFile represents a File for a Javascript value saved in local storage.
 type JsFile struct {
 	keyName string
-	fb      *filebuffer.Buffer
+	reader  *bytes.Reader
 	storage *jsStorage
+	dirty   bool
+	mux     sync.Mutex
 }
 
 // initFile creates a new in-memory file buffer of the key value.
 func initFile(keyName, keyValue string, storage *jsStorage) *JsFile {
 	f := &JsFile{
 		keyName: keyName,
-		fb:      filebuffer.New([]byte(keyValue)),
+		reader:  bytes.NewReader([]byte(keyValue)),
 		storage: storage,
+		dirty:   false,
 	}
 
 	return f
@@ -36,7 +41,11 @@ func initFile(keyName, keyValue string, storage *jsStorage) *JsFile {
 // be canceled and return immediately with an ErrClosed error.
 // Close will return an error if it has already been called.
 func (f *JsFile) Close() error {
-	return f.fb.Close()
+	f.mux.Lock()
+	defer f.mux.Unlock()
+
+	f.reader.Reset(nil)
+	return nil
 }
 
 // Name returns the name of the file as presented to Open.
@@ -48,7 +57,18 @@ func (f *JsFile) Name() string {
 // It returns the number of bytes read and any error encountered.
 // At end of file, Read returns 0, io.EOF.
 func (f *JsFile) Read(b []byte) (n int, err error) {
-	return f.fb.Read(b)
+	f.mux.Lock()
+	defer f.mux.Unlock()
+
+	if f.dirty {
+		keyValue, err := f.storage.Get(f.keyName)
+		if err != nil {
+			return 0, errors.Errorf("could not read %q: %+v", f.keyName, err)
+		}
+		f.reader.Reset([]byte(keyValue))
+	}
+
+	return f.reader.Read(b)
 }
 
 // ReadAt reads len(b) bytes from the File starting at byte offset off.
@@ -56,7 +76,18 @@ func (f *JsFile) Read(b []byte) (n int, err error) {
 // ReadAt always returns a non-nil error when n < len(b).
 // At end of file, that error is io.EOF.
 func (f *JsFile) ReadAt(b []byte, off int64) (n int, err error) {
-	return f.fb.ReadAt(b, off)
+	f.mux.Lock()
+	defer f.mux.Unlock()
+
+	if f.dirty {
+		keyValue, err := f.storage.Get(f.keyName)
+		if err != nil {
+			return 0, errors.Errorf("could not readAt %q: %+v", f.keyName, err)
+		}
+		f.reader.Reset([]byte(keyValue))
+	}
+
+	return f.reader.ReadAt(b, off)
 }
 
 // Seek sets the offset for the next Read or Write on file to offset,
@@ -69,14 +100,33 @@ func (f *JsFile) ReadAt(b []byte, off int64) (n int, err error) {
 // can seek to the beginning of the directory on Unix-like operating
 // systems, but not on Windows.
 func (f *JsFile) Seek(offset int64, whence int) (ret int64, err error) {
-	return f.fb.Seek(offset, whence)
+	f.mux.Lock()
+	defer f.mux.Unlock()
+
+	if f.dirty {
+		keyValue, err := f.storage.Get(f.keyName)
+		if err != nil {
+			return 0, errors.Errorf("could not seek %q: %+v", f.keyName, err)
+		}
+		f.reader.Reset([]byte(keyValue))
+	}
+
+	return f.reader.Seek(offset, whence)
 }
 
 // Sync commits the current contents of the file to stable storage.
 // Typically, this means flushing the file system's in-memory copy
 // of recently written data to disk.
 func (f *JsFile) Sync() error {
-	f.storage.Set(f.keyName, f.fb.String())
+	f.mux.Lock()
+	defer f.mux.Unlock()
+
+	keyValue, err := f.storage.Get(f.keyName)
+	if err != nil {
+		return errors.Errorf("could not sync %q: %+v", f.keyName, err)
+	}
+
+	f.reader.Reset([]byte(keyValue))
 	return nil
 }
 
@@ -84,7 +134,19 @@ func (f *JsFile) Sync() error {
 // It returns the number of bytes written and an error, if any.
 // Write returns a non-nil error when n != len(b).
 func (f *JsFile) Write(b []byte) (n int, err error) {
-	return f.fb.Write(b)
+	f.mux.Lock()
+	defer f.mux.Unlock()
+
+	f.dirty = true
+
+	keyValue, err := f.storage.Get(f.keyName)
+	if err != nil {
+		return 0, errors.Errorf("could not write to %q: %+v", f.keyName, err)
+	}
+
+	f.storage.Set(f.keyName, keyValue+string(b))
+
+	return len(b), nil
 }
 
 // JsFileInfo represents a FileInfo for a Javascript value saved in local
